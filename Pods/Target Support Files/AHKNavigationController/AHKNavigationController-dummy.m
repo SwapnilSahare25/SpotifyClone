40d3e365320b58a78535d3e7de580f30162481a2
Swapnil <swapnilsahare753@gmail.com>
2025-11-28 22:59:16 +0530

    Add .gitignore and remove unnecessary files

--- Pods/Target Support Files/AHKNavigationController/AHKNavigationController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AHKNavigationController : NSObject
-@end
-@implementation PodsDummy_AHKNavigationController
-@end
--- Pods/Target Support Files/RCPageControl/RCPageControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RCPageControl : NSObject
-@end
-@implementation PodsDummy_RCPageControl
-@end
